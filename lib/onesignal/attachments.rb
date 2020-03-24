6c307c11a466fb410cd033e35a1380b78cfe5a08
max <max10541@hotmail.com>
2020-03-25 01:16:16 +0300

    add a few more parameters

--- lib/onesignal/attachments.rb
+++ lib/onesignal/attachments.rb
@@ -2,11 +2,12 @@
 
 module OneSignal
   class Attachments
-    attr_reader :data, :url, :ios_attachments, :android_picture, :amazon_picture, :chrome_picture
+    attr_reader :data, :url, :app_url, :ios_attachments, :android_picture, :amazon_picture, :chrome_picture
 
-    def initialize data: nil, url: nil, ios_attachments: nil, android_picture: nil, amazon_picture: nil, chrome_picture: nil
+    def initialize data: nil, url: nil, app_url: nil, ios_attachments: nil, android_picture: nil, amazon_picture: nil, chrome_picture: nil
       @data = data
       @url = url
+      @app_url = app_url
       @ios_attachments = ios_attachments
       @android_picture = android_picture
       @amazon_picture = amazon_picture
@@ -17,6 +18,7 @@ module OneSignal
       {
         'data'               => @data.as_json(options),
         'url'                => @url,
+        'app_url'            => @app_url,
         'ios_attachments'    => @ios_attachments.as_json(options),
         'big_picture'        => @android_picture,
         'adm_big_picture'    => @amazon_picture,
--- lib/onesignal/notification.rb
+++ lib/onesignal/notification.rb
@@ -6,24 +6,29 @@ require 'onesignal/notification/headings'
 module OneSignal
   class Notification
     attr_reader :contents, :headings, :template_id, :included_segments, :excluded_segments,
-                :included_targets, :send_after, :attachments, :sounds, :buttons
+                :included_targets, :send_after, :delayed_option, :delivery_time_of_day, :ttl,
+                :attachments, :sounds, :buttons, :collapse_id
 
     def initialize **params
       unless params.include?(:contents) || params.include?(:template_id)
         raise ArgumentError, 'missing contents or template_id'
       end
 
-      @contents          = params[:contents]
-      @headings          = params[:headings]
-      @template_id       = params[:template_id]
-      @included_segments = params[:included_segments]
-      @excluded_segments = params[:excluded_segments]
-      @included_targets  = params[:included_targets]
-      @send_after        = params[:send_after].to_s
-      @attachments       = params[:attachments]
-      @filters           = params[:filters]
-      @sounds            = params[:sounds]
-      @buttons           = params[:buttons]
+      @contents             = params[:contents]
+      @headings             = params[:headings]
+      @template_id          = params[:template_id]
+      @included_segments    = params[:included_segments]
+      @excluded_segments    = params[:excluded_segments]
+      @included_targets     = params[:included_targets]
+      @send_after           = params[:send_after].to_s
+      @delayed_option       = params[:delayed_option].to_s
+      @delivery_time_of_day = params[:delivery_time_of_day].to_s
+      @ttl                  = params[:ttl]
+      @attachments          = params[:attachments]
+      @filters              = params[:filters]
+      @sounds               = params[:sounds]
+      @buttons              = params[:buttons]
+      @collapse_id          = params[:collapse_id]
     end
 
     def as_json options = {}
